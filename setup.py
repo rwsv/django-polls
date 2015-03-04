74703becc1d8d41221071ace2917fabec5a2df90
Ramon Vianna <ramonw@poli.ufrj.br>
2015-03-04 15:32:57 -0300

    app polls separada do projeto

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-polls',
+    version='0.1',
+    packages=['polls'],
+    include_package_data=True,
+    description='A simple Django app to conduct Web-based polls.',
+    long_description=README,
+    author='Ramon Vianna',
+    author_email='ramonwsv@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
